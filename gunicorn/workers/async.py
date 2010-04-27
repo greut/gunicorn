ac425ed8b7d9bfdb67abeee296d51a8e09f9f17a
benoitc <bchesneau@gmail.com>
2010-04-27 15:14:25 +0200

    naive working integration of simplehttp parser. async worker is slower than the non async actually for unkown reason and slower than with old parser.

--- gunicorn/workers/async.py
+++ gunicorn/workers/async.py
@@ -56,7 +56,7 @@ class AsyncWorker(Worker):
             return False
         try:
             environ = req.read()
-            if not environ or not req.parser.headers:
+            if not environ:
                 return False
             respiter = self.app(environ, req.start_response)
             if respiter == ALREADY_HANDLED:
@@ -66,7 +66,7 @@ class AsyncWorker(Worker):
             req.response.close()
             if hasattr(respiter, "close"):
                 respiter.close()
-            if req.parser.should_close:
+            if req.req.should_close():
                 return False
         except Exception, e:
             #Only send back traceback in HTTP in debug mode.
